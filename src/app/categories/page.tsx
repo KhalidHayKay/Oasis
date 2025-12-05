35fb09c40d70888130fb65b5d3ebc745790f76cf
Khalid <alamutukhalid@gmail.com>
2025-12-05 15:59:59 +0100

    fix: updated next to fix built in package error and fixed server component build errors

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,6 +1,8 @@
 import { categoryService } from '@/services/categoryService';
 import CategoriesView from '@/view/categories';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
 	const categories = await categoryService.all();
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import HomeView from '@/view/home';
 import { productService } from '@/services/productService';
 import { inspirationService } from '@/services/inspirationService';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
 	const [products, inspo] = await Promise.all([
 		productService.top(),
