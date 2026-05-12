3f69874cabfcb46de49c0c6697054dd4a8bc44b4
Alamutu Khalid <alamutukhalid@gmail.com>
2026-05-12 21:35:13 +0100

    fix: removed key from AuthDrawer instance which was causing abrupt drawer close

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -1,33 +1,29 @@
 interface AuthResponse {
-	message: string;
-	user: User;
+    message: string;
+    user: User;
 }
 
 interface LoginCredentials {
-	email: string;
-	password: string;
+    email: string;
+    password: string;
 }
 
 interface RegistrationData {
-	name: string;
-	email: string;
-	password: string;
-	password_confirmation: string;
-	terms: boolean;
+    name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+    terms: boolean;
 }
 
 interface VerifyEmailRequest {
-	email: string;
-	code: string;
-}
-
-interface ForgotPasswordRequest {
-	email: string;
+    email: string;
+    code: string;
 }
 
 interface ResetPasswordRequest {
-	email: string;
-	token: string;
-	password: string;
-	password_confirmation: string;
+    email: string;
+    token: string;
+    password: string;
+    password_confirmation: string;
 }
